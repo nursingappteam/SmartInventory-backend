3952791c94df5b1ba09f93932675d60bc03064a5
Glitch (inquisitive-tremendous-mistake) <none>
2021-05-21 19:27:03 +0000

    🏔👗 Checkpoint ./client.js:31317407/1743 ./package.json:31317407/167

--- client.js
+++
@@ -1,8 +0,0 @@
-console.log("hiyya");
-
-const axios = require("axios");
-
-axios.get("https://hello-sqlite-revamp.glitch.me/getDreams").then(response => {
-    console.log(response.data);
-  })
-  .catch(error => console.error(error));
