77437bfd3aa33852d3cccd10a2a4abbe87d30b76
Glitch (inquisitive-tremendous-mistake) <none>
2021-05-21 16:16:07 +0000

    📮🍿 Checkpoint ./README.md:31317407/591 ./package.json:31317407/827 ./server.js:31317407/2549

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Hello SQLite!
 
-This project includes a [Node.js](https://nodejs.org/en/about/) server script that uses a persistent [SQLite](https://www.sqlite.org) database. The app also includes a front-end web page that connects to the database. The user can see the current data and submit new records to the database.
+This project includes a [Node.js](https://nodejs.org/en/about/) server script that uses a persistent [SQLite](https://www.sqlite.org) database. The app also includes a front-end web page that connects to the database. The user can see the current data and submit new records to the database. 📊
 
 ## What's in this project?
 
@@ -12,9 +12,21 @@ This project includes a [Node.js](https://nodejs.org/en/about/) server script th
 
 ← `src/tbd`:
 
-
+← `.env`:
 
 When the app runs Glitch builds the database:
 
 ← `sqlite.db` is created and placed in the `.data` folder, a hidden directory whose contents aren’t copied when a project is remixed. You can see the contents of `.data` in the console by selecting __Tools__ >  __Logs__.
 
+## Next steps
+
+_tbd_
+
+![Glitch](https://cdn.glitch.com/a9975ea6-8949-4bab-addb-8a95021dc2da%2FLogo_Color.svg?v=1602781328576)
+
+## You built this with Glitch!
+
+[Glitch](https://glitch.com) is a friendly community where millions of people come together to build web apps and websites.
+
+- Need more help? [Check out our Help Center](https://help.glitch.com/) for answers to any common questions.
+- Ready to make it official? [Become a paid Glitch member](https://glitch.com/pricing) to boost your app with private sharing, more storage and memory, domains and more.
--- server.js
+++ server.js
@@ -1,25 +1,38 @@
-// server.js
-// where your node app starts
+/*
 
-// init project
-const express = require("express");
-const bodyParser = require("body-parser");
-const app = express();
+*/
+
 const fs = require("fs");
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
 
-// we've started you off with Express,
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
-
-// http://expressjs.com/en/starter/static-files.html
-app.use(express.static("public"));
+const path = require("path");
 
 // init sqlite db
 const dbFile = "./.data/sqlite.db";
 const exists = fs.existsSync(dbFile);
 const sqlite3 = require("sqlite3").verbose();
 const db = new sqlite3.Database(dbFile);
+
+// Require the fastify framework and instantiate it
+const fastify = require("fastify")({
+  // Set this to true for detailed logging:
+  logger: false
+});
+
+// Setup our static files
+fastify.register(require("fastify-static"), {
+  root: path.join(__dirname, "public"),
+  prefix: "/" // optional: default '/'
+});
+
+// fastify-formbody lets us parse incoming forms
+fastify.register(require("fastify-formbody"));
+
+// point-of-view is a templating manager for fastify
+fastify.register(require("point-of-view"), {
+  engine: {
+    handlebars: require("handlebars")
+  }
+});
 
 // if ./.data/sqlite.db does not exist, create it, otherwise print records to console
 db.serialize(() => {
@@ -46,20 +59,20 @@ db.serialize(() => {
 });
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", (request, response) => {
-  response.sendFile(`${__dirname}/views/index.html`);
+fastify.get("/", function(request, reply) {
+  reply.view(`/views/index.html`);
 });
 
 // endpoint to get all the dreams in the database
-app.get("/getDreams", (request, response) => {
+fastify.get("/getDreams", (request, reply) => {
   db.all("SELECT * from Dreams", (err, rows) => {
-    response.send(JSON.stringify(rows));
+    reply.send(JSON.stringify(rows));
   });
 });
 
 // endpoint to add a dream to the database
-app.post("/addDream", (request, response) => {
-  console.log(`add to dreams ${request.body.dream}`);
+fastify.post("/addDream", (request, reply) => {
+  console.log(`Add to dreams ${request.body.dream}`);
 
   // DISALLOW_WRITE is an ENV variable that gets reset for new projects
   // so they can write to the database
@@ -67,16 +80,16 @@ app.post("/addDream", (request, response) => {
     const cleansedDream = cleanseString(request.body.dream);
     db.run(`INSERT INTO Dreams (dream) VALUES (?)`, cleansedDream, error => {
       if (error) {
-        response.send({ message: "error!" });
+        reply.send({ message: "error!" });
       } else {
-        response.send({ message: "success" });
+        reply.send({ message: "success" });
       }
     });
   }
 });
 
 // endpoint to clear dreams from the database
-app.get("/clearDreams", (request, response) => {
+fastify.get("/clearDreams", (request, reply) => {
   // DISALLOW_WRITE is an ENV variable that gets reset for new projects so you can write to the database
   if (!process.env.DISALLOW_WRITE) {
     db.each(
@@ -91,9 +104,9 @@ app.get("/clearDreams", (request, response) => {
       },
       err => {
         if (err) {
-          response.send({ message: "error!" });
+          reply.send({ message: "error!" });
         } else {
-          response.send({ message: "success" });
+          reply.send({ message: "success" });
         }
       }
     );
@@ -105,7 +118,12 @@ const cleanseString = function(string) {
   return string.replace(/</g, "&lt;").replace(/>/g, "&gt;");
 };
 
-// listen for requests :)
-var listener = app.listen(process.env.PORT, () => {
-  console.log(`Your app is listening on port ${listener.address().port}`);
+// Run the server and report out to the logs
+fastify.listen(process.env.PORT, function(err, address) {
+  if (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+  console.log(`Your app is listening on ${address}`);
+  fastify.log.info(`server listening on ${address}`);
 });
