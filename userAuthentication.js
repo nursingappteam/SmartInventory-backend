6e3a4cd9404776966d9f463fc2fea200b76ad116
Glitch (glitch-hello-sqlite) <none>
2022-11-01 01:51:47 +0000

    🎑💼 Checkpoint ./userAuthentication.js:49461564/432

--- userAuthentication.js
+++ userAuthentication.js
@@ -1,19 +1,31 @@
 const argon2 = require("argon2");
 
-let createUser = (username, password) => {
+let createUserQuery = (username, password, user_type) => {
   let hash;
   try{
     const salt = Math.random().toString(36).substring(2,7);
     let hash = argon2.hash(password, {salt: salt});
     console.log("HASH: ", hash);
-    let query = `INSERT INTO USERS`
+    return `
+      INSERT INTO users (user_id, user_name, user_pass_secure, user_type_id, user_enabled, salt,   register_date)
+      VALUES(
+        NULL,
+        '${username}',
+        '${hash}',
+        1,
+        1,
+        '${salt}',
+        datetime('now','localtime')
+      )
+      
+    `
     
   } catch(err) {
     
   }
 }
 
-let verifyUser = () => {
+let verifyUserQuery = () => {
   
 }
 
