4a80185e45137713f2701ead2b9cb0444968f946
Glitch (glitch-hello-sqlite) <none>
2022-11-01 01:29:21 +0000

    🏕🏟 Checkpoint ./userAuthentication.js:49461564/315

---
+++ userAuthentication.js
@@ -0,0 +1,16 @@
+const argon2 = require("argon2");
+
+let createUser = (username, password) => {
+  let hash;
+  try{
+    
+  } catch(err) {
+    
+  }
+}
+
+let verifyUser = () => {
+  
+}
+
+module.export = {createUser, verifyUser};
