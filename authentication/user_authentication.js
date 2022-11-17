c7bfc11d0b5cd2947c6b26718779e69330d8f25c
kmosoti <kennedy.mosoti@mavs.uta.edu>
2022-11-17 10:17:53 -0600

    Deleted Main Branch

--- authentication/user_authentication.js
+++ authentication/user_authentication.js
@@ -1,15 +1,15 @@
 var md5 = require('md5');
 
-let createUserQuery = (username, password, user_type) => {
+let createUserQuery = (username, password, email, user_type) => {
   let hash;
   try{
     const salt = Math.random().toString(36).substring(2,7);
     let hash = md5(password+salt);
     //console.log("HASH: ", hash);
     return `
-      INSERT INTO users (user_id,user_name,user_pass_secure,user_type_id,user_enabled,salt,register_date)
-      VALUES(NULL,'${username}','${hash}',1,1,'${salt}',datetime('now','localtime'))`
-    
+      INSERT INTO users (user_id, user_email, user_name, user_pass_secure, user_type_id, user_enabled, salt, register_date)
+      VALUES (NULL, '${email}', '${username}', '${hash}', '${user_type}', '1', '${salt}', CURRENT_TIMESTAMP);
+    `
   } catch(err) {
     console.log();
     return ``
