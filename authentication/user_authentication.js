89d9039c7f2aca276c3d89ce1200ebed6864e84b
kmosoti <kennedy.mosoti@mavs.uta.edu>
2022-11-17 11:43:51 -0600

    Changed hash function to bcrypt and uninstalled md5

--- authentication/user_authentication.js
+++ authentication/user_authentication.js
@@ -1,35 +1,4 @@
-var md5 = require('md5');
 const bcrypt = require('bcrypt');
-
-// let createUserQuery = (username, password, email, user_type) => {
-//   let hash;
-//   try{
-//     const salt = Math.random().toString(36).substring(2,7);
-//     let hash = md5(password+salt);
-//     //console.log("HASH: ", hash);
-//     return `
-//       INSERT INTO users (user_id, user_email, user_name, user_pass_secure, user_type_id, user_enabled, salt, register_date)
-//       VALUES (NULL, '${email}', '${username}', '${hash}', '${user_type}', '1', '${salt}', CURRENT_TIMESTAMP);
-//     `
-//   } catch(err) {
-//     console.log(err);
-//     return ``
-//   }
-// }
-// let verifyUserQuery = (username, password, salt) => {
-//   try{
-//     let hash = md5(password+salt);
-//     //console.log("HASH: ", hash);
-//     return `
-//     SELECT user_id, user_name, user_type_id, user_enabled, register_date FROM users WHERE user_name = '${username}'
-//     AND user_pass_secure = '${hash}'
-//     `
-//   } catch(err){
-//     console.log(err);
-//     return ``
-//   }
-// }
-
 // Create user with bcrypt
 let createUserQuery = (username, password, email, user_type) => {
   //Use bcrypt syncronously salt and hash password
